61219bbffa33889c07031fa2f24868ff4193e5ea
kaushik <kaushikam@gmail.com>
2022-05-28 14:32:12 +0530

    formatted

--- main.go
+++ main.go
@@ -22,8 +22,10 @@ func main() {
 	totalTimeout := questions.TotalTimeout()
 	fmt.Printf("Here are your questions (All should be answered in %s): \n", totalTimeout)
 	totTimer := time.NewTimer(totalTimeout)
+
 	report := make(chan string)
 	go askQuestions(questions, report)
+
 	select {
 	case <-totTimer.C:
 		fmt.Println("Quiz time is over")
@@ -35,23 +37,16 @@ func main() {
 	}
 }
 
-func getAnswer(reader *bufio.Reader, a chan string) {
-	answer, err := reader.ReadString('\n')
-	if err != nil {
-		a <- "INVALID"
-		return
-	}
-	a <- answer
-}
-
 func askQuestions(questions quiz.Questions, c chan string) {
 	reader := bufio.NewReader(os.Stdin)
 	answer := make(chan string)
 
 	for i, q := range questions {
 		q.Display(i + 1)
+
 		timer := time.NewTimer(q.Timeout())
 		go getAnswer(reader, answer)
+
 		select {
 		case <-timer.C:
 			fmt.Println("Answering timed out")
@@ -63,4 +58,13 @@ func askQuestions(questions quiz.Questions, c chan string) {
 	}
 
 	c <- questions.Result()
+}
+
+func getAnswer(reader *bufio.Reader, a chan string) {
+	answer, err := reader.ReadString('\n')
+	if err != nil {
+		a <- "INVALID"
+		return
+	}
+	a <- answer
 }
